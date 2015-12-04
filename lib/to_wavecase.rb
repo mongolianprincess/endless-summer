36730825da4732ef672ce71853c8398a5c8d8d64
mongolianprincess <ayc.yang@gmail.com>
2015-12-04 11:15:37 +0000

    first commit

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,4 +1,17 @@
 class String
 
-
+  def to_wavecase(string)
+    sentence = Array.new
+    string.split('').each do |word|
+      string.each_char.with_index do |c, index|
+        if index.even?
+          sentence.push(c.downcase)
+        else
+          sentence.push(c.upcase)
+        end
+      end
+    end
+  end
 end
+
+puts to_wavecase("Colourless green ideas sleep furiously")
