ddb9cde6b3511382f80d7d0e08060e0be933e87c
mongolianprincess <ayc.yang@gmail.com>
2015-12-04 11:33:01 +0000

    final

--- lib/to_wavecase.rb
+++ lib/to_wavecase.rb
@@ -1,19 +1,6 @@
 class String
 
-  def to_wavecase string
-   arr, char, result = [],[],[]
-   arr = string.split(' ').each_slice(string.split(' ').count).to_a.transpose
-   arr.each_with_index do |arr_word, index|
-     arr_word.each_with_index do |word, sub_index|
-       word.each_char.with_index do |letter, letter_index|
-         letter_index.even? ? char.push(letter.upcase) : char.push(letter.downcase)
-       end
-     end
-     result.push(char.join(''))
-     char.clear
-   end
-   result.join(' ')
+  def to_wavecase
+    self.gsub(/\w/).with_index{|s,i|i.even?? s.downcase : s.upcase}
   end
-end
-
-puts to_wavecase("Colourless green ideas sleep furiously")
+end 
